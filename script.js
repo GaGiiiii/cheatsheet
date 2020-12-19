a53be8162d8fbdc4c0833690b684eb9cf14edd07
GaGiiiii <dragoslav.gagi8@gmail.com>
2020-12-19 19:58:09 +0100

    Added menu

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let pres = document.querySelectorAll("pre.collapse");
-console.log(pres);
+// console.log(pres);
 let buttons = document.querySelectorAll(".cBTN");
-console.log(buttons);
+// console.log(buttons);
 
-for(let i = 0; i < pres.length; i++){
+for (let i = 0; i < pres.length; i++) {
   pres[i].id = "collapseExample" + i;
   buttons[i].dataset.target = "#collapseExample" + i;
   buttons[i].setAttribute("aria-controls", "collapseExample" + i);
 }
+
+
+let menuLis = [...document.querySelectorAll('#menu a.list-group-item')];
+let windowsHash = window.location.hash;
+let croppedUrl = windowsHash.substr(1);
+let lastActive = menuLis.find(element => element.dataset.menuItem == croppedUrl);
+lastActive.classList.add('active');
+
+menuLis.forEach((menuLi) => {
+  menuLi.addEventListener('click', (event) => {
+    if (lastActive) {
+      lastActive.classList.remove('active');
+    }
+    menuLi.classList.add('active');
+    lastActive = menuLi;
+  });
+});
