cf05fcc5b6a6381b8e502b4339e8979a35cc62c1
GaGiiiii <dragoslav.gagi8@gmail.com>
2022-07-12 16:38:27 +0200

    Upgraded Laravel Generate Commands

--- script.js
+++ script.js
@@ -58,19 +58,164 @@ copyBtns.forEach((copyBtn, index) => {
 let laravelGenerateTerminalCommandsForm = document.getElementById('laravel-generate-terminal-commands-form');
 let resourceNameInput = document.getElementById('resource-name');
 let generateCommandsBtn = document.getElementById('generate-commands-btn');
+let laravelCheckBoxes = document.querySelectorAll('#laravel-generate-terminal-commands-form input[type=checkbox]');
+let projectLocationDiv = document.getElementById('project-location-div');
+let projectLocationInput = document.getElementById('project-location-input');
+let laravelCheckboxAll = document.getElementById('laravel-checkbox-all');
+let numberOfChecked = 0;
+
+laravelCheckBoxes.forEach(laravelCheckBox => {
+    laravelCheckBox.addEventListener('change', (e) => {
+        if (laravelCheckBox.value === 'all') {
+            if (laravelCheckBox.checked) {
+                laravelCheckBoxes.forEach(laravelCheckBox => {
+                    laravelCheckBox.checked = true;
+                });
+            } else {
+                laravelCheckBoxes.forEach(laravelCheckBox => {
+                    laravelCheckBox.checked = false;
+                });
+            }
+
+            numberOfChecked = 1; // Because next if will increment it to 2 or decrease to 0
+        }
+
+        if (!laravelCheckBox.checked) {
+            laravelCheckboxAll.checked = false;
+        }
+
+        if (laravelCheckBox.value === 'service' || laravelCheckBox.value === 'dto' || laravelCheckBox.value === 'all') {
+            if (laravelCheckBox.checked) {
+                projectLocationDiv.classList.remove('d-none');
+                projectLocationDiv.classList.add('d-block');
+                numberOfChecked++; // Hey I am the next if that will increment it to 2 :)
+            } else {
+                numberOfChecked--; // Hey I am the next if that will decrease it to 0 :)
+                if (numberOfChecked < 1) {
+                    projectLocationDiv.classList.add('d-none');
+                    projectLocationDiv.classList.remove('d-block');
+                }
+            }
+        }
+
+        console.log(numberOfChecked)
+    });
+});
 
 laravelGenerateTerminalCommandsForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let resourceName = resourceNameInput.value;
-    console.log(resourceName)
-    let text = `php artisan make:migration create_${resourceName.toLowerCase()}s_table && \
-php artisan make:controller ${resourceName}/${resourceName}Controller --api && \
-php artisan make:model ${resourceName} && \
-php artisan make:request ${resourceName}/Create${resourceName}Request && \
+    let text = '';
+
+    if (laravelCheckboxAll.checked) {
+        //         text += `php artisan make:migration create_${resourceName.toLowerCase()}s_table\\\n\
+        // && mkdir ${projectLocationInput.value}/app/Data/${resourceName}\\\n\
+        // && touch ${projectLocationInput.value}/app/Data/${resourceName}/${resourceName}Data.php\\\n\
+        // && mkdir ${projectLocationInput.value}/app/Services/${resourceName}\\\n\
+        // && touch ${projectLocationInput.value}/app/Services/${resourceName}/${resourceName}Service.php\\\n\
+        // && php artisan make:controller ${resourceName}/${resourceName}Controller --api\\\n\
+        // && php artisan make:request ${resourceName}/Create${resourceName}Request\\\n\
+        // && php artisan make:request ${resourceName}/Update${resourceName}Request\\\n\
+        // && php artisan make:request ${resourceName}/Delete${resourceName}Request\\\n\
+        // && php artisan make:request ${resourceName}/Get${resourceName}Request\\\n\
+        // && php artisan make:request ${resourceName}/GetAll${resourceName}sRequest\\\n\
+        // && php artisan make:factory ${resourceName}/${resourceName}Factory\\\n\
+        // && php artisan make:test ${resourceName}/${resourceName}Test\\\n\
+        // && php artisan make:test ${resourceName}/${resourceName}Test --unit`;
+        text += `php artisan make:migration create_${resourceName.toLowerCase()}s_table\
+&& php artisan make:model ${resourceName}\
+&& mkdir ${projectLocationInput.value}/app/Data/${resourceName}\
+&& touch ${projectLocationInput.value}/app/Data/${resourceName}/${resourceName}Data.php\
+&& mkdir ${projectLocationInput.value}/app/Services/${resourceName}\
+&& touch ${projectLocationInput.value}/app/Services/${resourceName}/${resourceName}Service.php\
+&& php artisan make:controller ${resourceName}/${resourceName}Controller --api\
+&& php artisan make:request ${resourceName}/Create${resourceName}Request\
+&& php artisan make:request ${resourceName}/Update${resourceName}Request\
+&& php artisan make:request ${resourceName}/Delete${resourceName}Request\
+&& php artisan make:request ${resourceName}/Get${resourceName}Request\
+&& php artisan make:request ${resourceName}/GetAll${resourceName}sRequest\
+&& php artisan make:factory ${resourceName}/${resourceName}Factory\
+&& php artisan make:test ${resourceName}/${resourceName}Test\
+&& php artisan make:test ${resourceName}/${resourceName}Test --unit`;
+    } else {
+        laravelCheckBoxes.forEach(laravelCheckBox => {
+            if (laravelCheckBox.checked) {
+                switch (laravelCheckBox.value) {
+                    case 'migration':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:migration create_${resourceName.toLowerCase()}s_table`;
+
+                        break;
+                    case 'model':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:model ${resourceName}`;
+
+                        break;
+                    case 'dto':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `mkdir ${projectLocationInput.value}/app/Data/${resourceName}`;
+                        text += ` && touch ${projectLocationInput.value}/app/Data/${resourceName}/${resourceName}Data.php`;
+
+                        break;
+                    case 'service':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `mkdir ${projectLocationInput.value}/app/Services/${resourceName}`;
+                        text += ` && touch ${projectLocationInput.value}/app/Services/${resourceName}/${resourceName}Service.php`;
+
+                        break;
+                    case 'controller':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:controller ${resourceName}/${resourceName}Controller --api`;
+
+                        break;
+                    case 'requests':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:request ${resourceName}/Create${resourceName}Request && \
 php artisan make:request ${resourceName}/Update${resourceName}Request && \
 php artisan make:request ${resourceName}/Delete${resourceName}Request && \
 php artisan make:request ${resourceName}/Get${resourceName}Request && \
 php artisan make:request ${resourceName}/GetAll${resourceName}sRequest`;
+
+                        break;
+                    case 'factory':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:factory ${resourceName}/${resourceName}Factory`;
+
+                        break;
+                    case 'feature':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:test ${resourceName}/${resourceName}Test`;
+
+                        break;
+                    case 'unit':
+                        if (text.length > 0) {
+                            text += ' && ';
+                        }
+                        text += `php artisan make:test ${resourceName}/${resourceName}Test --unit`;
+
+                        break;
+                }
+            }
+        });
+    }
+
+    console.log(text)
 
     /* Copy the text inside the text field */
     navigator.clipboard.writeText(text);
@@ -79,3 +224,4 @@ php artisan make:request ${resourceName}/GetAll${resourceName}sRequest`;
         generateCommandsBtn.innerHTML = "Generate commands";
     }, 1000);
 });
+
